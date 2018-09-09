9d7167bc80c31a33c069eb6efcbf9b8e35b06f9e
Jules Fakhoury <jules.fakhoury@gmail.com>
2018-09-09 23:29:49 +0200

    class model, functions to add, update, delete items

--- web_app.py
+++ web_app.py
@@ -1,6 +1,7 @@
 
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from decorators import require_request_params
 
 
 app = Flask(__name__)
@@ -9,6 +10,14 @@ db = SQLAlchemy(app)
 
 
 class ToDoItems(db.Model):
+    """
+    Class used to represent to-do items
+
+    Attributes:
+        (int) id: id of item
+        (str) text: content of item
+        (bool) done: True if item completed, otherwise False
+    """
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
     done = db.Column(db.Boolean)
@@ -16,13 +25,21 @@ class ToDoItems(db.Model):
 
 @app.route('/')
 def index():
+    """render template with list of all to-do items"""
     items = ToDoItems.query.all()
     return render_template('index.html', items=items)
 
 
 @app.route('/add/item/', methods=['POST'])
-def add_item():
-    item_text = request.form.get('item')
+@require_request_params(['item_text'])
+def add_item(item_text):
+    """
+    Function to add a new item with content from "item_text"
+
+    :param (str) item_text: content of item
+
+    :return: redirect to root page
+    """
     if item_text:
         todo_item = ToDoItems(text=item_text, done=False)
         db.session.add(todo_item)
@@ -31,15 +48,31 @@ def add_item():
 
 
 @app.route('/update/item/<item_id>', methods=['POST'])
-def update_item(item_id):
+@require_request_params(['done'])
+def update_item(item_id, done):
+    """
+    Function to mark an item as done (if "done" is True) or undone (if "done" is False)
+
+    :param (int) item_id: id of item to update
+    :param (bool or str) done: True if item completed, otherwise False
+
+    :return: redirect to root page
+    """
     todo_item = ToDoItems.query.filter_by(id=int(item_id)).first()
-    todo_item.done = bool(request.form.get('done'))
+    todo_item.done = bool(done)
     db.session.commit()
     return redirect(url_for('index'))
 
 
 @app.route('/delete/item/<item_id>')
 def delete_item(item_id):
+    """
+    Function to delete a to-do item based on its id
+
+    :param (int) item_id: id of item to delete
+
+    :return: redirect to root page
+    """
     todo_item = ToDoItems.query.filter_by(id=int(item_id)).first()
     db.session.delete(todo_item)
     db.session.commit()
